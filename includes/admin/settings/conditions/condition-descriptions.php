36099794a81d8411fdd380759376703cb1c1bdeb
JeroenSormani <jeroen.sormani@gmail.com>
2016-01-30 17:29:00 +0100

    Code style changes

--- includes/admin/settings/conditions/condition-descriptions.php
+++ includes/admin/settings/conditions/condition-descriptions.php
@@ -8,23 +8,23 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
  *
  * @since 1.0.0
  *
- * @param string $condition Current condition to display the description for.
+ * @param  string  $condition  Current condition to display the description for.
  */
 function wcasv_condition_description( $condition ) {
 
 	$descriptions = array(
-		'shipping_cost' 			=> __( 'Shipping cost is based on the shipping package cost (taxes not counted)', 'woocommerce-advanced-shipping-validation' ),
-		'state' 					=> __( 'States must be installed in WC', 'woocommerce-advanced-shipping-validation' ),
-		'zipcode' 					=> __( 'Zipcodes can be separated by comma. Will match when user zipcode \'starts with\' any of the provided zipcodes.', 'woocommerce-advanced-shipping-validation' ),
-		'city' 					    => __( 'City can be separated by comma. Case incentive', 'woocommerce-advanced-shipping-validation' ),
-		'weight' 					=> __( 'Weight calculated on all the cart contents', 'woocommerce-advanced-shipping-validation' ),
-		'length' 					=> __( 'Compared to lengthiest product in cart', 'woocommerce-advanced-shipping-validation' ),
-		'width' 					=> __( 'Compared to widest product in cart', 'woocommerce-advanced-shipping-validation' ),
-		'height'					=> __( 'Compared to highest product in cart', 'woocommerce-advanced-shipping-validation' ),
-		'stock_status' 				=> __( 'All products in cart must match stock status', 'woocommerce-advanced-shipping-validation' ),
-		'contains_category' 		=> __( 'Cart must contain least one product with the selected category', 'woocommerce-advanced-shipping-validation' ),
-		'contains_product' 			=> __( 'Cart must contain one of this product', 'woocommerce-advanced-shipping-validation' ),
-		'contains_shipping_class' 	=> __( 'Cart must contain at least one product with the selected shipping class', 'woocommerce-advanced-shipping-validation' ),
+		'shipping_cost'           => __( 'Shipping cost is based on the shipping package cost (taxes not counted)', 'woocommerce-advanced-shipping-validation' ),
+		'state'                   => __( 'States must be installed in WC', 'woocommerce-advanced-shipping-validation' ),
+		'zipcode'                 => __( 'Zipcodes can be separated by comma. Will match when user zipcode \'starts with\' any of the provided zipcodes.', 'woocommerce-advanced-shipping-validation' ),
+		'city'                    => __( 'City can be separated by comma. Case incentive', 'woocommerce-advanced-shipping-validation' ),
+		'weight'                  => __( 'Weight calculated on all the cart contents', 'woocommerce-advanced-shipping-validation' ),
+		'length'                  => __( 'Compared to lengthiest product in cart', 'woocommerce-advanced-shipping-validation' ),
+		'width'                   => __( 'Compared to widest product in cart', 'woocommerce-advanced-shipping-validation' ),
+		'height'                  => __( 'Compared to highest product in cart', 'woocommerce-advanced-shipping-validation' ),
+		'stock_status'            => __( 'All products in cart must match stock status', 'woocommerce-advanced-shipping-validation' ),
+		'contains_category'       => __( 'Cart must contain least one product with the selected category', 'woocommerce-advanced-shipping-validation' ),
+		'contains_product'        => __( 'Cart must contain one of this product', 'woocommerce-advanced-shipping-validation' ),
+		'contains_shipping_class' => __( 'Cart must contain at least one product with the selected shipping class', 'woocommerce-advanced-shipping-validation' ),
 	);
 	$descriptions = apply_filters( 'woocommerce_Advanced_Shipping_Validation_descriptions', $descriptions );
 
