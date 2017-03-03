1ca6687fbd14b69254c5f22ce20758fbfd69d0ee
JeroenSormani <jeroen.sormani@gmail.com>
2017-03-03 20:53:08 +0100

    Code consistency changes for libraries/wp-conditions/conditions/wpc-contains-product-condition.php

--- libraries/wp-conditions/conditions/wpc-contains-product-condition.php
+++ libraries/wp-conditions/conditions/wpc-contains-product-condition.php
@@ -6,13 +6,16 @@ if ( ! class_exists( 'WPC_Contains_Product_Condition' ) ) {
 	class WPC_Contains_Product_Condition extends WPC_Condition {
 
 		public function __construct() {
+
 			$this->name        = __( 'Contains product', 'wpc-conditions' );
 			$this->slug        = __( 'contains_product', 'wpc-conditions' );
 			$this->group       = __( 'Cart', 'wpc-conditions' );
 			$this->description = __( 'Check if a product is or is not present in the cart', 'wpc-conditions' );
 
 			parent::__construct();
+
 		}
+
 
 		public function match( $match, $operator, $value ) {
 
@@ -28,6 +31,7 @@ if ( ! class_exists( 'WPC_Contains_Product_Condition' ) ) {
 
 		}
 
+
 		public function get_compare_value() {
 
 			$product_ids = array();
@@ -42,6 +46,7 @@ if ( ! class_exists( 'WPC_Contains_Product_Condition' ) ) {
 
 		}
 
+
 		public function get_available_operators() {
 
 			$operators = parent::get_available_operators();
@@ -53,16 +58,17 @@ if ( ! class_exists( 'WPC_Contains_Product_Condition' ) ) {
 
 		}
 
+
 		// @todo
 		public function get_value_field_args() {
 
 			$field_args = array(
-				'type' => 'text',
+				'type'              => 'text',
 				'custom_attributes' => array(
 					'data-placeholder' => __( 'Search for a product', 'wp-conditions' ),
 				),
-				'class' => array( 'wpc-value', 'wc-product-search' ),
-				'options' => array(),
+				'class'             => array( 'wpc-value', 'wc-product-search' ),
+				'options'           => array(),
 			);
 
 			if ( version_compare( WC()->version, '2.7', '>=' ) ) {
@@ -72,6 +78,7 @@ if ( ! class_exists( 'WPC_Contains_Product_Condition' ) ) {
 			return $field_args;
 
 		}
+
 
 	}
 
